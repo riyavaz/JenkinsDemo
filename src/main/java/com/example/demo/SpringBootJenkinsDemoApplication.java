7e3dbe997661907a53e51469f0247055116fbfbd
riyavaz <riyavaz12@gmail.com>
2021-08-24 22:28:32 +0530

    first commit

---
+++ src/main/java/com/example/demo/SpringBootJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJenkinsDemoApplication.class, args);
+	}
+
+}
